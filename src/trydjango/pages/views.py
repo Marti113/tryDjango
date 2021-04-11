e8080d697772bb7f12ae21a56388d37ee6268669
Marti Kandallu <marti.kandallu@gmail.com>
2021-04-11 11:08:09 -0700

    Inital commit

---
+++ src/trydjango/pages/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>Hello World</h1>")
+    my_context = {
+        "my_text": "This is about me",
+        "my_number": 123,
+        "my_list" : [13, 31, 71]
+    }
+    return render(request, "home.html", my_context)
