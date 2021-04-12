1e44f991a0a3ab1a1622d4a46ea7e78fdd6fae90
Marti Kandallu <marti.kandallu@gmail.com>
2021-04-12 14:00:50 -0700

    Implemented search form

--- src/trydjango/products/views.py
+++ src/trydjango/products/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 from .forms import ProductForm
 from .models import Product
 
+def product_create_view(request):
+    context = {}
+    return render(request, 'products/product_create.html', context)
+
 
 # Create your views here.
-def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context = {
-        'form': form
-    }
-    return render(request, 'products/product_create.html', context)
+#     def product_create_view(request):
+#         form = ProductForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+#             form = ProductForm()
+#         context = {
+#             'form': form
+#         }
+#         return render(request, 'products/product_create.html', context)
 
 
 def product_detail_view(request):
